70d76659336d3aedbde0f27bcdea7f75dc873b31
Jeff Rossi <jrossi@intouchgps.com>
2022-05-21 09:24:31 -0400

    ormconfig not found by start

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,16 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
-  type: 'mysql',
-	host: process.env.DB_HOST,
-	port: 3306,
-	username: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME,
-  entities: ['./dist/**/*.entity.js'],
-  migrations: ['./dist/migrations/**/*.js'],
-	cli: {
-    migrationsDir: './dist/migrations',
-    subscribersDir: './dist/subscribers',
+  "type": "mysql",
+	"host": process.env.DB_HOST,
+	"port": 3306,
+	"username": process.env.DB_USER,
+	"password": process.env.DB_PASS,
+	"database": process.env.DB_NAME,
+  "entities": ["./dist/**/*.entity.js"],
+  "migrations": ["./dist/migrations/**/*.js"],
+	"cli": {
+    "migrationsDir": "./dist/migrations",
+    "subscribersDir": "./dist/subscribers",
 	},
-  synchronize: false,
+  "synchronize": false,
 }
